481e6c8401de46e0c5e3285db6309513c185f75b
Nirajmahi <mahiummeniraj1@gmail.com>
2022-05-17 23:46:33 +0600

    branchA variable added

--- file2.java
+++ file2.java
@@ -2,6 +2,11 @@ package com.example.new_sample;
 
 public class file2 {
     public String name;
+    public String branchA;
+
+    public String getBranchA() {
+        return branchA;
+    }
 
     public file2(String name) {
         this.name = name;
