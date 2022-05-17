c555fd06cd89aa99d7e62ad4de1bf7ee41b38586
Nirajmahi <mahiummeniraj1@gmail.com>
2022-05-17 23:15:08 +0600

    new file with getter setter constructor

---
+++ file1.java
@@ -0,0 +1,17 @@
+package com.example.new_sample;
+
+public class file1 {
+    public String name;
+
+    public file1(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
